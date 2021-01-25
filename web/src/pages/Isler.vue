64e04b9844151c3f2a6dc43f7e822d988279a506
Okan Sahin <okansahinfed@gmail.com>
2021-01-25 03:15:58 +0300

    isler.vue added

---
+++ web/src/pages/Isler.vue
@@ -0,0 +1,77 @@
+<template>
+  <Layout :show-logo="false">
+    <!-- Author intro -->
+
+    <!-- List posts -->
+    <div class="posts">
+      <post-card
+        v-for="edge in $page.posts.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+        :metadata="$page.metadata"
+      />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+{
+  metadata {
+    sanityOptions {
+      projectId
+      dataset
+    }
+  }
+  posts: allSanityPost(sortBy: "publishedAt") {
+    edges {
+      node {
+        id
+        title
+        slug {
+          current
+        }
+        categories {
+          id
+          title
+        }
+        publishedAt(format: "D. MMMM YYYY")
+        _rawExcerpt
+        mainImage {
+          asset {
+            _id
+            url
+          }
+          caption
+          alt
+          hotspot {
+            x
+            y
+            height
+            width
+          }
+          crop {
+            top
+            bottom
+            left
+            right
+          }
+        }
+      }
+    }
+  }
+}
+
+</page-query>
+
+<script>
+import PostCard from '~/components/PostCard'
+
+export default {
+  components: {
+    PostCard
+  },
+  metaInfo: {
+    title: 'İşlerimiz'
+  }
+}
+</script>
