16b2538250a2e162b06cd650d169298dd7aa0011
Okan Sahin <okansahinfed@gmail.com>
2021-01-25 13:11:18 +0300

    header component created

---
+++ web/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  
+<div class="container mx-auto p-5">
+    <nav class="flex justify-between items-center max-h-28">
+            <HeaderLogo />
+        <ul class="flex flex-row">
+            <li class="pr-5"><a> Services </a></li>
+            <li class="pr-5"><a>Porfolio</a></li>
+            <li class="pr-5"><a>About</a></li>
+            <li><a>Contact</a></li>
+        </ul>
+    </nav>
+</div>
+</template>
+
+<script>
+import HeaderLogo from '~/components/HeaderLogo'
+
+export default {
+  components: {
+    HeaderLogo
+  },
+}
+</script>
+
+<style>
+
+</style>
