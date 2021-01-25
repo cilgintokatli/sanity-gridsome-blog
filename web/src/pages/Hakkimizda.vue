c3ffb0419dd08a422b48b6e9b47b64d93bf3cd3f
Okan Sahin <okansahinfed@gmail.com>
2021-01-25 13:09:17 +0300

    about page updated

---
+++ web/src/pages/Hakkimizda.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout :show-logo="true">
+    <!-- Author intro -->
+    <div>
+        hakkımızda!!!!
+    </div>
+  </Layout>
+</template>
+
+<script>
+
+export default {
+  metaInfo: {
+    title: 'Hakkımızda!'
+  }
+}
+</script>
