7e8af7c226a20702f5804de92e61d37ea7b27845
Okan Sahin <okansahinfed@gmail.com>
2021-01-25 16:20:31 +0300

    contact form added

--- web/src/pages/Iletisim.vue
+++ web/src/pages/Iletisim.vue
@@ -1,7 +1,40 @@
 <template>
   <Layout>
     <div>
-        iletişim
+        <form
+            name="contact"
+            method="post"
+            v-on:submit.prevent="handleSubmit"
+            action="/iletisim/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            netlify
+            >
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+                <label>
+                Don’t fill this out: <input name="bot-field" />
+                </label>
+            </p>
+            <div class="sender-info">
+                <div>
+                <label for="name" class="label" >İsim</label>
+                <input type="text" name="name" v-model="formData.name" />
+                </div>
+                <div>
+                <label for="email">E mail</label>
+                <input type="email" name="email" v-model="formData.email" />
+                </div>
+            </div>
+
+            <div class="message-wrapper">
+                <label for="message">Mesaj</label>
+                <textarea name="message" v-model="formData.message"></textarea>
+            </div>
+
+            <button type="submit">Gönder</button>
+        </form>
+
     </div>
   </Layout>
 </template>
@@ -11,6 +44,30 @@
 export default {
   metaInfo: {
     title: 'İletişim'
-  }
+  },
+  data() {
+        return {
+            formData: {},
+        }
+    },
+    methods: {
+        encode(data) {
+            return Object.keys(data)
+            .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+            .join('&')
+        },
+        handleSubmit(e) {
+            fetch('/iletisim', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: this.encode({
+                    'form-name': e.target.getAttribute('name'),
+                    ...this.formData,
+                }),
+            })
+            .then(() => console.log('Form successfully submitted'))
+            .catch(error => alert(error))
+        }
+    },
 }
 </script>
