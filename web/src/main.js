4a7c32fb09ef51c6da85902f44ec6fdaa8922316
Okan Sahin <okansahinfed@gmail.com>
2021-01-27 03:24:34 +0300

    vimeo player plugin added to global

--- web/src/main.js
+++ web/src/main.js
@@ -7,6 +7,8 @@ import DefaultLayout from '~/layouts/Default.vue'
 // Import image url builder
 import urlForImage from './utils/urlForImage'
 
+import vueVimeoPlayer from 'vue-vimeo-player'
+
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
@@ -14,4 +16,7 @@ export default function(Vue, { router, head, isClient }) {
 
   // Inject global image URL builder
   Vue.prototype.$urlForImage = urlForImage
+
+  Vue.use(vueVimeoPlayer)
+
 }
